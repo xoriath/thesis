02508614de40a7d723a8c93b4490adb8758e819c
xoriath <moro.engelhardt@gmai.com>
2013-03-06 15:07:56 +0100

    started on webcam

--- Filter3/WebcameraImageSource.h
+++ Filter3/WebcameraImageSource.h
@@ -9,14 +9,20 @@ class WebcameraImageSource : public AbstractLinearMovieSource
 {
     Q_OBJECT
 public:
-    explicit WebcameraImageSource(QWidget *parent = 0);
-
-signals:
-    
-public slots:
+    explicit WebcameraImageSource(int source, QWidget *parent = 0);
+    explicit WebcameraImageSource(QString source, QWidget *parent = 0);
+    virtual bool resume();
+    virtual bool stop();
     
 private:
     QWidget *parent;
+
+public slots:
+    virtual bool capture();
+
+signals:
+    void captured(cv::Mat image);
+
 };
 
 #endif // WEBCAMERAIMAGESOURCE_H
