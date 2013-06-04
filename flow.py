3a4cd7c0c660b05f11c67b4a91b3619e896e6987
xoriath <moro.engelhardt@gmail.com>
2013-06-04 12:08:45 +0200

    Implement Farneback2003 flow algo

--- flow.py
+++ flow.py
@@ -4,6 +4,7 @@ import cv2
 import numpy
 
 def draw_flow(im, flow, step=16):
+	print flow
 	h, w = im.shape[:2]
 	y, x = numpy.mgrid[step/2:h:step, step/2:w:step].reshape(2, -1)
 	fx, fy = flow[y,x].T
@@ -17,19 +18,35 @@ def draw_flow(im, flow, step=16):
 		cv2.circle(vis, (x1, y1), 1, (0,255,0), -1)
 	return vis
 	
-cap = cv2.VideoCapture('/home/moro/Dropbox/Litteratursøk/video/veggtest_600_1m.mp4')
+cap = cv2.VideoCapture('/home/moro/Dropbox/Litteratursøk/video/referanse_fart.mp4')
 
 ret, im = cap.read()
+im = numpy.asarray(im[:,:])
 prev_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 
 while True:
 	ret, im = cap.read()
+	im = numpy.asarray(im[:,:])
 	gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-	
-	flow = cv2.calcOpticalFlowFarneback(prev_gray,gray,0.5,1,3,15,3,5,1,cv2.OPTFLOW_USE_INITIAL_FLOW)
+	flow = numpy.array((10,10), dtype=numpy.int32)
+	flow = cv2.calcOpticalFlowFarneback(
+		prev=prev_gray, 
+		next=gray,
+		flow=None,
+		pyr_scale=float(0.5), 
+		levels=1, 
+		winsize=9, 
+		iterations=1, 
+		poly_n=7, 
+		poly_sigma=float(1.5),
+		flags=cv2.OPTFLOW_FARNEBACK_GAUSSIAN)
 	prev_gray = gray
 
     # plot the flow vectors
 	cv2.imshow('Optical flow',draw_flow(gray,flow))
-	if cv2.waitKey(10) == 27:
+	if cv2.waitKey(1) == 27:
+		print "BREAK"
 		break
+	if cv2.waitKey(1) == 'r':
+		print "RESET"
+		flow = numpy.array((10,10), dtype=numpy.int32)
