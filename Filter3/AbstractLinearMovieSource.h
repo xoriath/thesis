1c8adfaecc4925d1aa4120c2153d9ada1c08a8ab
xoriath <moro.engelhardt@gmail.com>
2013-07-07 21:46:58 +0200

    Big rework of thing

--- Filter3/AbstractLinearMovieSource.h
+++
@@ -1,9 +0,0 @@
-#ifndef ABSTRACTLINEARMOVIESOURCE_H
-#define ABSTRACTLINEARMOVIESOURCE_H
-
-#include "AbstractImageSource.h"
-#include "AbstractPauseable.h"
-
-class AbstractLinearMovieSource : public AbstractImageSource, public AbstractPauseable {};
-
-#endif // ABSTRACTLINEARMOVIESOURCE_H
